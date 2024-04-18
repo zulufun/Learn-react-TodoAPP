971ddf5496c8898f95e17fa95eb7a294e183178a
zulufun <ducphucuuu@gmail.com>
2024-04-18 16:38:06 +0700

    Update lib and atlaskit/button, textfield, icon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Todolist from "./components/Todolist";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+    <h3>Danh sach can lam</h3>
+
+    
+    );
 }
 
 export default App;
